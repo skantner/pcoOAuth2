cb6100b0caa5e948089f40d61636b1cca57ad47c
skantner <spkantner@gmail.com>
2018-02-19 12:51:18 -0500

    initial project setup

--- pcoOAuth2/ViewController.swift
+++ pcoOAuth2/ViewController.swift
@@ -9,17 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    
+    func iTunesURL(searchText: String) -> URL {
+        let encodedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        let urlString = String(format:"https://itunes.apple.com/search?term=%@", encodedText)
+        let url = URL(string: urlString)
+        return url!
+    }
+    
+    
+    @IBAction func goPressed() {
+        let url = iTunesURL(searchText: "Hillsong")
+        print ("URL: '\(url)'")
+        if let data = performStoreRequest(with: url) {
+            let results = parse(data: data)
+            print ("Results: '\(results)'")
+        }
+        
+        
+    }
+    
+    func performStoreRequest(with url: URL) -> Data? {
+        do {
+            return try Data(contentsOf: url)
+        } catch {
+            print("Download error: \(error.localizedDescription)")
+            showNetworkError()
+            return nil
+            
+        }
+    }
+    
+    func parse(data: Data) -> [SearchResult] {
+        do {
+            let decoder = JSONDecoder()
+            let result = try decoder.decode(ResultArray.self, from: data)
+            return result.results
+        } catch {
+            print("JSON error: \(error)")
+            return []
+        }
+    }
+    
+    func showNetworkError() {
+        let alert = UIAlertController(title: "Whoops...",
+                                      message: "There was an error accessing the iTunes Store. Please try again.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        present(alert, animated: true, completion: nil)
+        alert.addAction(action)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 

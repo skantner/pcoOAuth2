972f22503a8274f22f0d2fd6cb4e07a780c20909
skantner <spkantner@gmail.com>
2018-03-06 14:51:24 -0500

    iterim commit - PlanItem class

---
+++ pcoOAuth2/PlanItem.swift
@@ -0,0 +1,9 @@
+//
+//  PlanItem.swift
+//  pcoOAuth2
+//
+//  Created by Scott Kantner on 3/6/18.
+//  Copyright © 2018 Kantner Research And Technology. All rights reserved.
+//
+
+import Foundation
